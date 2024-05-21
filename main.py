42ed96dc2e340b73c8eec5b9c588da8db8e4cf1f
burak <rburaksaritas@gmail.com>
2024-05-22 00:38:14 +0300

    Added support for ignoring specific files
    - Added a new optional argument `--ignore` to the command-line interface in `main.py` to specify specific files to ignore when generating commit messages.
    - Updated the `generate_commits` function call to include the new `args.ignore` parameter, allowing for the exclusion of specified files from commit message generation.

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate commit messages for modified files in a git repository.")
     parser.add_argument("repo_path", help="Path to the git repository")
     parser.add_argument("--file", nargs='+', help="Specific files to generate commits for")
+    parser.add_argument("--ignore", nargs='+', help="Specific files to ignore")
     parser.add_argument("--push", help="Branch name to push the changes")
 
     args = parser.parse_args()
     
-    generate_commits(args.repo_path, args.file)
+    generate_commits(args.repo_path, args.file, args.ignore)
 
     if args.push:
         git_push(args.repo_path, args.push)
