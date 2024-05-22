1973ca0685dfe2ed7ddfbbd74a97cd9a6631d2dd
burak <rburaksaritas@gmail.com>
2024-05-22 03:10:27 +0300

    Update commit message handling in pipeline.py
    - Updated the handling of commit message generation and file modification in the `generate_commits` function in `pipeline.py`.
    - Adjusted the logic to properly check for the generated commit message before proceeding.
    - Added a message indicating the successful generation of the commit message.
    - Implemented proper handling for committing modified files, including a success message for each successfully committed file.

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,6 +2,7 @@ import os
 import difflib
 from .ai_integration import generate_commit_message, parse_commit_message
 from .git_operations import get_modified_files, get_file_content, get_file_diff, git_add, git_commit, git_rm, file_exists_in_commit
+
 GREEN = "\033[92m"
 RESET = "\033[0m"
 
@@ -62,12 +63,13 @@ def generate_commits(repo_path, specific_files=None, ignored_files=None):
         title, message = parse_commit_message(commit_message_response)
 
         if title and message:
-            print(f"{GREEN}----Commit Generated----{RESET}", "\n", title, "\n\n", message, "\n", f"{GREEN}-----------------------{RESET}")
+            print(title, "\n\n", message, "\n", f"{GREEN}Commit message generated successfully.{RESET}")
             if is_deleted_file:
                 git_rm(repo_path, file_path)
             else:
                 git_add(repo_path, file_path)
             git_commit(repo_path, title, message)
+            print(f"{GREEN}Modified file {file_path} is commited successfully.{RESET}")
         else:
             print("No commit message generated. Please try again.")
             exit(1)
